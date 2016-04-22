db563e1bdb54e1882400b5484afd2bb9cd4e41f9
feipyang <feipyang@gmail.com>
2016-04-22 21:28:20 +0800

    After debug

--- rsync.sh
+++ rsync.sh
@@ -5,11 +5,10 @@ mirror="archive.kernel.org/centos-vault"
 # Local Repository Location
 repobase="/home/igw/img/Repo/CentOS7"
 
-
-# versions could be added like ver1,ver2, etc..
+# versions could be added like ver1 ver2 etc, seperate by space
 for vers in 7.0.1406; do
 	mkdir -p ${repobase}/${vers}/{cr,centosplus,extras,fasttrack,os,updates}/x86_64
-	for repo in cr,centosplus,extras,fasttrack,os,updates; do
+	for repo in cr centosplus extras fasttrack os updates; do
 		rsync -art rsync://${mirror}/${vers}/$repo/x86_64 ${repobase}/${vers}/os/
 		if [ $? -ne 0 ]; then
 			echo "ERROR getting ${repo} files from ${mirror} for ${vers} x64, quitting."

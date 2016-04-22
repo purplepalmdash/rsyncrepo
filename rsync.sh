78752dec9d3ac00d47e636651df44c3c846a6367
feipyang <feipyang@gmail.com>
2016-04-22 21:32:27 +0800

    Re-Debug

--- rsync.sh
+++ rsync.sh
@@ -7,8 +7,8 @@ repobase="/home/igw/img/Repo/CentOS7"
 
 # versions could be added like ver1 ver2 etc, seperate by space
 for vers in 7.0.1406; do
-	mkdir -p ${repobase}/${vers}/{cr,centosplus,extras,fasttrack,os,updates}/x86_64
 	for repo in cr centosplus extras fasttrack os updates; do
+		mkdir -p ${repobase}/${vers}/${repo}/x86_64
 		rsync -art rsync://${mirror}/${vers}/$repo/x86_64 ${repobase}/${vers}/os/
 		if [ $? -ne 0 ]; then
 			echo "ERROR getting ${repo} files from ${mirror} for ${vers} x64, quitting."

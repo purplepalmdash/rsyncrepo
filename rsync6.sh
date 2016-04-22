df97c1d9c7b194d2d0ab33d1b6859ba75ea9b7c7
feipyang <feipyang@gmail.com>
2016-04-22 21:58:25 +0800

    Added 6

--- rsync6.sh
+++ rsync6.sh
@@ -3,11 +3,11 @@
 # kernel.org supports rsync
 mirror="archive.kernel.org/centos-vault"
 # Local Repository Location
-repobase="/home/igw/img/Repo/CentOS7"
+repobase="/home/igw/img/Repo/CentOS6"
 
 # versions could be added like ver1 ver2 etc, seperate by space
-for vers in 7.0.1406; do
-	for repo in cr centosplus extras fasttrack os updates; do
+for vers in 6.4; do
+	for repo in cr centosplus contrib extras fasttrack os updates xen4; do
 		mkdir -p ${repobase}/${vers}/${repo}/x86_64
 		rsync -artv rsync://${mirror}/${vers}/$repo/x86_64 ${repobase}/${vers}/os/
 		if [ $? -ne 0 ]; then
